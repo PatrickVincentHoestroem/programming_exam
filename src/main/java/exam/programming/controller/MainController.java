5f52148da418fb3a059f02116b6b7eb37e78b0e4
PatrickVincentHoestroem <patrick.vincent@live.dk>
2021-06-19 22:56:33 +0200

    Update 1 after delivery

--- src/main/java/exam/programming/controller/MainController.java
+++ src/main/java/exam/programming/controller/MainController.java
@@ -1,13 +1,9 @@
 package exam.programming.controller;
 
-import exam.programming.model.Parish;
 import exam.programming.repository.ParishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class MainController {
@@ -16,9 +12,7 @@ public class MainController {
     ParishRepository parishRepository;
 
     @GetMapping("/")
-    public String index(Model model) {
-        List<Parish> l = (List<Parish>) parishRepository.findAll();
-        model.addAttribute("parishes", l);
+    public String index() {
         return "index";
     }
 }
